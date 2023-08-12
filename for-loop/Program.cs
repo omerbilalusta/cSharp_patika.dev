ddf319041acfe4b51d1e781bee114003c5bf2d36
Omer Bilal Usta <omerbilalusta53@gmail.com>
2023-08-12 14:23:20 +0300

    if_else, switch_case, for_loop

---
+++ for-loop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayac; i++)
+            {
+                if(i%2 ==1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            int tekToplam=0;
+            int ciftToplam=0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if(i%2==1)
+                {
+                    tekToplam+=i;
+                }
+                else
+                {
+                    ciftToplam+=i;
+                }
+            }
+            Console.WriteLine("Tek sayıların toplamı= "+tekToplam);
+            Console.WriteLine("Cift sayilarin toplamı= "+ ciftToplam);
+
+            //break, continue
+            for (int i = 1; i < 10; i++)
+            {
+                if (i==4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ switch-case/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındayız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındayız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındayız");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos ayındayız");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı veri");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimindeyiz");
+                    break;
+                
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimindeyiz");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
