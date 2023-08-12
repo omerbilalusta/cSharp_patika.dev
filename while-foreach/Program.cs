504f2718bc7228c7217d6eed8251a1f39b394e93
Omer Bilal Usta <omerbilalusta53@gmail.com>
2023-08-12 14:25:06 +0300

    while_loop

---
+++ while-foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1'den başlayarak yazılan sayıya kadarki sayıların ortalaması
+            Console.WriteLine("Sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+
+            // 'a' dan z'ye kadar tüm harfleri konsola yazdırma
+            char chrachter='a';
+            while (chrachter<= 'z')
+            {
+                Console.Write(chrachter);
+                chrachter++;
+            }
+
+            Console.WriteLine("\n******** Foreach ******");
+            string[] arabalar = {"BMW", "Ford", "Audi", "GTR"};
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
