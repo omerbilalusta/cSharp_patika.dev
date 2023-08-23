30504d750330f6c39581e2a63a7f5b23c481ac2c
Omer Bilal Usta <73158243+usta5353@users.noreply.github.com>
2023-08-23 15:24:48 +0300

    KolaySeviyeProjeler

---
+++ c#_Proje/karakterTerstenYazdırma/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Cümle giriniz.");
+            string input = Console.ReadLine();
+            foreach (var item in input.Split(' '))
+            {
+                foreach (var reversedItem in item.Reverse())
+                {
+                    Console.Write(reversedItem);
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
