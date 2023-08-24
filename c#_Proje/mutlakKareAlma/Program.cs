93a48b41d9c6c46b1e49a064280f0591736186f1
Omer Bilal Usta <73158243+usta5353@users.noreply.github.com>
2023-08-24 16:08:14 +0300

    Orta Seviye Projeler

---
+++ c#_Proje/mutlakKareAlma/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace mutlakKareAlma
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Değer giriniz");
+            string input = Console.ReadLine(), keep="";
+            List<int> list = new List<int>();
+            string [] array = input.Split(' ');
+            
+            
+            int smalls=0;
+            double bigs=0; 
+            foreach (var item in array)
+            {
+                if(Int32.Parse(item) > 67)
+                    bigs += Math.Pow((Int32.Parse(item)-67),2);
+                else
+                    smalls += 67 - Int32.Parse(item);
+            }
+            Console.WriteLine("Büyükler: {1} Küçükler: {0}", bigs, smalls);
+        }
+    }
+}
---
+++ c#_Proje/sessizHarf/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace sessizHarf
+{
+    class Program
+    {
+        static void Main(string[] args){
+            char [] sesliHarf = new char[]{'a','e','ı','i','o','ö','u','ü','A','E','I','İ','O','Ö','U','Ü'};
+            Console.WriteLine("Cümle giriniz: ");
+            string input = Console.ReadLine();
+            bool triger = false, triger2 = false;
+            char keep='0';
+            foreach (var item in input.Split())
+            {
+                triger = false;
+                foreach (var item2 in item.ToCharArray())
+                {
+                    if(!sesliHarf.Contains(keep) && !sesliHarf.Contains(item2)){
+                        if(triger2==true){
+                            Console.Write("True ");
+                            triger = true;
+                            triger2 = false;
+                            break;
+                        }
+                        triger2=true;
+                        keep = item2;
+                    }
+                    else
+                        triger2=false;
+                }
+                if(triger == false)
+                    Console.Write("False ");
+            }
+        }
+    }
+}
