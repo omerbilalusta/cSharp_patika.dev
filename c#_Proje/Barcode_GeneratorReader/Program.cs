4eca0d7c954904522a72ffc046e26be1a1d3b20f
Omer Bilal Usta <73158243+usta5353@users.noreply.github.com>
2023-08-28 15:28:16 +0300

    Zor Seviye Projeler
    runtime folder deleted due large size(88 MB) at location ../bin/Debug/Runtime

---
+++ c#_Proje/Barcode_GeneratorReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using IronBarCode;
+using System;
+using System.Linq;
+
+namespace BarcodeProgram
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Barcode haline getirilmesini istediğiniz metni giriniz.");
+            GeneratedBarcode qrCode = createQR(Console.ReadLine());
+            readQR(qrCode);
+
+        }
+        
+        static GeneratedBarcode createQR(string value){
+            QRCodeLogo qRCodeLogo = new QRCodeLogo("patika.dev_logo.png");
+            GeneratedBarcode qrCode =  QRCodeWriter.CreateQrCodeWithLogo(value,qRCodeLogo);
+            qrCode.ResizeTo(500,500).SetMargins(10);
+            qrCode.SaveAsPng("qrCode.png");
+            return qrCode;
+        }
+        static void readQR(GeneratedBarcode qrCode){
+            var resultOfQrCode = BarcodeReader.Read("qrCode.png").First();
+            if(qrCode.BinaryValue.SequenceEqual(resultOfQrCode.BinaryValue)){
+                Console.WriteLine(resultOfQrCode.Text);
+            }
+            else
+                Console.WriteLine("Bozulmuş data.");
+        }
+    }
+}
